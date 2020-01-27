60da23e216e7ba1f006b9acd47ac8343b64b166c
Agund <agund1988@gmail.com>
2020-01-27 14:52:32 +0100

    init
    init

---
+++ Blazor Native/MyApp/MyApp.Android/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Support.V4.Content;
+
+namespace MyApp.Droid
+{
+    [Activity(Label = "MyApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            
+            
+            base.OnCreate(savedInstanceState);
+            int requestPermissions = 1;
+            string cameraPermission = Android.Manifest.Permission.Camera;
+            if (!(ContextCompat.CheckSelfPermission(this, cameraPermission) == (int)Permission.Granted))
+            {
+                ActivityCompat.RequestPermissions(this, new String[] { cameraPermission, }, requestPermissions);
+            }
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Blazor Native/MyApp/MyApp/HelloWorld.razor.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.MobileBlazorBindings.Elements;
+using Plugin.Media;
+
+namespace MyApp
+{
+    public class VHalloWorld : ComponentBase
+    {
+        public Image imageData { get; set; } = new Image();
+        public bool KameraAktiv { get; set; } = true;
+        public string path { get; set; } = "";
+        public string filename { get; set; } = "";
+        public List<string> fotoliste { get; set; } = new List<string>();
+        public List<int> selectDelete { get; set; } = new List<int>();
+
+        public void GetFotos()
+        {
+            fotoliste.Clear();
+            foreach (var file in Directory.GetFiles("/storage/emulated/0/Android/data/com.companyname/files/Pictures/Pictures"))
+            {
+                if (file.Contains(".jpg"))
+                {
+                    fotoliste.Add(file);
+                }
+            }
+            StateHasChanged();
+            
+        }
+
+        public void DelFile(string f)
+        {
+            
+            File.Delete(f);
+            GetFotos();
+        }
+
+        public void DeleteAll()
+        {
+            foreach (var foto in fotoliste)
+            {
+             File.Delete(foto);   
+            }
+            GetFotos();
+        }
+        public async Task GetFoto()
+        {
+            try
+            {
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    KameraAktiv = false;
+                    StateHasChanged();
+                    return;
+                }
+
+                var p = "/storeage/emulated/0/DCIM/"; // System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyPictures);/storeage/emulated/0/DCIM/
+                path = p;
+                //if (!Directory.Exists(p))
+                //    Directory.CreateDirectory(p);
+
+
+                var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                });
+
+
+                if (file == null)
+                    return;
+
+                //  imageData.Source = ImageSource.FromStream(() =>
+                //  {
+                //      var stream = file.GetStream();
+                //      file.Dispose();
+                //      return stream;
+                //  });
+
+
+                fotoliste.Add(file.Path);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+        public async Task CreateFoto()
+        {
+            try
+            {
+                await CrossMedia.Current.Initialize();
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    KameraAktiv = false;
+                    StateHasChanged();
+                    return;
+                }
+
+
+
+                var file = await CrossMedia.Current.TakePhotoAsync(
+                    new Plugin.Media.Abstractions.StoreCameraMediaOptions()
+                    {
+                        PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+                        Directory = "Pictures",
+                        Name = "test.jpg",
+                        SaveToAlbum = false
+                    });
+
+                if (file == null)
+                    return;
+
+                filename = file.Path;
+                GetFotos();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+
+        }
+    }
+    }
